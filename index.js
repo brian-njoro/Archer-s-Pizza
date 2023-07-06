73e4e4865ace4609f06f0dbbd90dc9c189464666
Brian NJoroge <brian.njoroge1@student.moringaschool.com>
2023-07-06 22:03:47 +0300

    add event and function to large checkbox in index.js

--- index.js
+++ index.js
@@ -11,3 +11,24 @@ smallCheckbox.addEventListener(`click`,function() {
     console.log(`total price:`, pizzaTotal)
 })
 
+// price if medium pizza selected
+let mediumCheckbox = document.getElementById(`cbMedium`)
+let mediumPrice = 750
+mediumCheckbox.addEventListener(`click`,function() {
+    if (mediumCheckbox.checked) {
+        //checkbox is checked, add medium pizza Price to Total price
+        pizzaTotal += mediumPrice
+    }
+    console.log(`total price:`, pizzaTotal)
+})
+
+//Price if large pizza selected
+let largeCheckbox = document.getElementById(`cbLarge`)
+let largePrice = 900
+largeCheckbox.addEventListener(`click`,function() {
+    if (largeCheckbox.checked) {
+        //checkbox is checked, add large pizza Price to Total price
+        pizzaTotal += largePrice
+    }
+    console.log(`total price:`, pizzaTotal)
+})
